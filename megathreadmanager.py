5055ea63fc3fea38cb4d89f1f79aff4b82bbc58c
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2021-01-20 18:40:35 -0600

    Further refactor session objects and cache current thread

--- megathreadmanager.py
+++ megathreadmanager.py
@@ -15,7 +15,7 @@ import praw
 import prawcore.exceptions
 
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 # General constants
 CONFIG_DIRECTORY = Path("~/.config/megathread_manager").expanduser()
@@ -47,26 +47,32 @@ class ConfigError(RuntimeError):
     pass
 
 
+class MegathreadUserSession:
+    """Cached state specific to a Reddit user."""
+
+    def __init__(self, config, credential_key):
+        self.reddit = praw.Reddit(**{
+            **config["praw_credentials"],
+            **config[credential_key],
+            **{"user_agent": USER_AGENT},
+            })
+        self.subreddit = self.reddit.subreddit(
+            config["subreddit_name"])
+        self.wiki_page = self.subreddit.wiki[
+            config["wiki_page_name"]]
+        self.current_thread = self.reddit.submission(
+            url=config["thread_url"]) if config["thread_url"] else None
+
+
 class MegathreadSession:
     """Common cached state for managing megathreads."""
 
     def __init__(self, config):
         self.config = config
-        self.reddit_mod = praw.Reddit(**{
-            **config["praw_credentials"],
-            **config["praw_credentials_mod"],
-            **{"user_agent": USER_AGENT},
-            })
-        self.reddit_post = praw.Reddit(**{
-            **config["praw_credentials"],
-            **config["praw_credentials_post"],
-            **{"user_agent": USER_AGENT},
-            })
-        self.subreddit_post = self.reddit_post.subreddit(
-            config["subreddit_name"])
-        self.subreddit_mod = self.reddit_mod.subreddit(
-            config["subreddit_name"])
-        self.wiki_page = self.subreddit_post.wiki[config["wiki_page_name"]]
+        self.user = MegathreadUserSession(
+            config=config, credential_key="praw_credentials_post")
+        self.mod = MegathreadUserSession(
+            config=config, credential_key="praw_credentials_mod")
 
 
 def write_config(config, config_path=CONFIG_PATH):
@@ -101,19 +107,18 @@ def generate_post_details(session):
         "current_datetime_local": datetime.datetime.now(),
         "subreddit_name": session.config["subreddit_name"],
         "thread_number": session.config["thread_number"],
+        "thread_url": session.config["thread_url"],
         }
     post_title = session.config["post_title_template"].format(
         **template_variables)
-    post_text = session.wiki_page.content_md.format(**template_variables)
+    post_text = session.user.wiki_page.content_md.format(**template_variables)
     return {"title": post_title, "selftext": post_text}
 
 
 def update_current_thread(session):
     """Update the text of the current thread."""
     post_details = generate_post_details(session)
-    current_thread = session.reddit_post.submission(
-        url=session.config["thread_url"])
-    current_thread.edit(post_details["selftext"])
+    session.user.current_thread.edit(post_details["selftext"])
 
 
 def create_new_thread(session):
@@ -121,19 +126,18 @@ def create_new_thread(session):
     session.config["thread_number"] += 1
     post_details = generate_post_details(session)
 
-    new_thread = session.subreddit_post.submit(**post_details)
+    new_thread = session.user.subreddit.submit(**post_details)
     new_thread.disable_inbox_replies()
-    new_thread_mod = session.reddit_mod.submission(url=new_thread.url)
+    new_thread_mod = session.mod.reddit.submission(url=new_thread.url)
     new_thread_mod.mod.approve()
 
     if session.config["pin_thread"]:
         thread_url = session.config["thread_url"]
         bottom_sticky = session.config["pin_thread"] != "top"
         if thread_url:
-            session.reddit_mod.submission(url=thread_url).mod.sticky(
-                state=False)
+            session.mod.current_thread.mod.sticky(state=False)
         try:
-            sticky_to_keep = session.subreddit_mod.sticky(number=1)
+            sticky_to_keep = session.mod.subreddit.sticky(number=1)
         except prawcore.exceptions.NotFound:
             sticky_to_keep = None
         new_thread_mod.mod.sticky(state=True, bottom=bottom_sticky)
@@ -141,19 +145,19 @@ def create_new_thread(session):
             sticky_to_keep.mod.sticky(state=True)
 
     session.config["thread_url"] = new_thread.url
+    session.user.current_thread = new_thread
+    session.mod.current_thread = new_thread_mod
 
 
 def manage_thread(session):
     """Manage the current thread, creating or updating it as nessesary."""
-    wiki_updated = (session.wiki_page.revision_date
+    wiki_updated = (session.user.wiki_page.revision_date
                     > session.config["wiki_page_timestamp"])
     interval = session.config["new_thread_interval"]
     last_post_timestamp = 0
     if session.config["thread_url"]:
-        current_thread = session.reddit_mod.submission(
-            url=session.config["thread_url"])
         last_post_timestamp = datetime.datetime.fromtimestamp(
-            current_thread.created_utc, tz=datetime.timezone.utc)
+            session.user.current_thread.created_utc, tz=datetime.timezone.utc)
     should_post_new_thread = not last_post_timestamp or (
         interval and (getattr(last_post_timestamp, interval)
                       != getattr(CURRENT_DATETIME, interval)))
@@ -163,7 +167,8 @@ def manage_thread(session):
             create_new_thread(session)
         else:
             update_current_thread(session)
-        session.config["wiki_page_timestamp"] = session.wiki_page.revision_date
+        session.config["wiki_page_timestamp"] = (
+            session.user.wiki_page.revision_date)
 
 
 def run_manage(config_path=CONFIG_PATH):
